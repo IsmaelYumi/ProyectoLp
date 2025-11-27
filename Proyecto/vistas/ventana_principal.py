ccc30f5e4885ff20b919f77e23c08036c09eca9c
Ismael <93888943+IsmaelYumi@users.noreply.github.com>
2025-11-27 01:31:31 -0500

    merge

--- Proyecto/vistas/ventana_principal.py
+++ Proyecto/vistas/ventana_principal.py
@@ -1,8 +1,9 @@
+import os
+import sys
+sys.path.insert(0, os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
 import tkinter as tk
 from tkinter import ttk, filedialog, messagebox
 from controladores.controller import AnalizadorController
-import os
-
 class VentanaPrincipal(tk.Tk):
 
     def __init__(self):
