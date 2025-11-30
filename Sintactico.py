9a3e68d1200f03f8c3ffdd479ffe19324cf0f72d
Ismael Yumipanta <93888943+IsmaelYumi@users.noreply.github.com>
2025-11-29 20:11:34 -0500

    SINTACTICO AGREGADO

---
+++ Sintactico.py
@@ -0,0 +1,220 @@
+import ply.yacc as yacc
+from Lexico import tokens, lexer
+import os
+
+start = "program"
+
+#Funcion para logs
+def log(level, message):
+    os.makedirs("./Logs", exist_ok=True)
+    ruta = f"./Logs/Sintactico-{lexer.nombre_archivo}-{lexer.fecha_actual}.txt"
+    linea= f" [{level}] {message}"
+    with open(ruta, "a+", encoding="utf-8") as f:
+        f.write(linea + "\n")
+
+
+# ===========================
+# PROGRAMA
+# ===========================
+def p_program(p):
+    """program : using_list class_section"""
+    pass
+
+def p_using_list(p):
+    """using_list : using_list using_section
+                  | using_section
+                  | empty"""
+    pass
+
+def p_using_section(p):
+    """using_section : USING SYSTEM SEMICOLON"""
+    pass
+
+# ===========================
+# CLASE
+# ===========================
+def p_class_section(p):
+    """class_section : CLASS ID LBRACE class_body RBRACE 
+                     | CLASS PROGRAM LBRACE class_body RBRACE """
+
+    log("OK","Expresion verificada , nombre de clase correcto:"f"{p[1]},")
+    pass
+
+def p_class_body(p):
+    """class_body : class_body class_member
+                  | class_member
+                  | empty"""
+    pass
+
+def p_class_member(p):
+    """class_member : method
+                    | property"""
+    pass
+
+# ===========================
+# PROPIEDADES
+# ===========================
+def p_property(p):
+    """property : type ID SEMICOLON"""
+    log("OK","Expresion verificada , propiedad declarada correctamente:"f"{p[1]}")
+    pass
+
+# ===========================
+# TIPOS
+# ===========================
+def p_type(p):
+    """type : INT
+            | DOUBLE
+            | CHAR
+            | STRINGTYPE
+            | VOID"""
+    p[0] = p[1]
+    log("OK","Expresion verificada , Tipo delcarado correctamente:"f"{p[0]}")
+
+# ===========================
+# MÉTODOS
+# ===========================
+def p_method(p):
+    """
+    method : STATIC VOID ID LPAREN RPAREN LBRACE statements RBRACE
+           | STATIC VOID ID LPAREN parameter_list RPAREN LBRACE statements RBRACE
+    """
+    log("OK","Expresion verificada , metodo delcarado correctamente:"f"{p[3]}")
+    pass
+
+# ===========================
+# PARÁMETROS
+# ===========================
+def p_parameter_list(p):
+    """parameter_list : param
+                      | parameter_list COMMA param"""
+    pass
+
+def p_param(p):
+    """param : type ID"""
+    log("OK","Expresion verificada , parametro declarado correctamente:"f"{p[3]}")
+    pass
+
+# ===========================
+# STATEMENTS
+# ===========================
+def p_statements(p):
+    """statements : statements statement
+                  | statement
+                  | empty"""
+    pass
+
+def p_statement(p):
+    """statement : declaration SEMICOLON
+                 | assignment SEMICOLON
+                 | method_call SEMICOLON
+                 | expression SEMICOLON
+                 | if_statement
+                 | while_statement"""
+    log("OK","Expresion verificada , declaracion  correcta"f"{p[1]}")
+    pass
+
+ 
+# ===========================
+# DECLARACIÓN / ASIGNACIÓN
+# ===========================
+def p_declaration(p):
+    """declaration : type ID EQUAL expression"""
+    log("OK","Expresion verificada , declaracion  correcta"f"{p[3]}")
+    pass
+
+def p_assignment(p):
+    """assignment : ID EQUAL expression"""
+    pass
+
+# ===========================
+# IF / WHILE
+# ===========================
+def p_if_statement(p):
+    """if_statement : IF LPAREN expression RPAREN LBRACE statements RBRACE
+                    | IF LPAREN expression RPAREN LBRACE statements RBRACE ELSE LBRACE statements RBRACE"""
+    log("OK","Expresion verificada , declaracion  correcta"f"{p[0]}{p[1]}{p[2]}")
+    pass
+
+def p_while_statement(p):
+    """while_statement : WHILE LPAREN expression RPAREN LBRACE statements RBRACE"""
+    pass
+
+# ===========================
+# CALLS
+# ===========================
+def p_method_call(p):
+    """method_call : ID LPAREN args RPAREN
+                   | ID DOT ID LPAREN args RPAREN
+                   | CONSOLE DOT ID LPAREN args RPAREN"""
+    pass
+
+def p_expression_method_call(p):
+    "expression : method_call"
+    pass
+
+
+def p_args(p):
+    """args : expression
+            | args COMMA expression
+            | empty"""
+    pass
+
+# ===========================
+# EXPRESIONES
+# ===========================
+def p_expression_group(p):
+    """expression : LPAREN expression RPAREN"""
+    p[0] = p[2]
+
+def p_expression_value(p):
+    """expression : ID
+                  | NUMBER
+                  | STRING_LITERAL"""
+    pass
+
+def p_expression_binop(p):
+    """expression : expression PLUS expression
+                  | expression MINUS expression
+                  | expression TIMES expression
+                  | expression DIVIDE expression
+                  | expression MOD expression"""
+    pass
+
+def p_expression_compare(p):
+    """expression : expression COMPARE expression
+                  | expression NOTEQUAL expression"""
+    pass
+
+def p_expression_logic(p):
+    """expression : expression AND expression
+                  | expression OR expression"""
+    pass
+
+def p_expression_dot(p):
+    """expression : expression DOT ID"""
+    pass
+
+
+
+
+# ===========================
+# EMPTY
+# ===========================
+def p_empty(p):
+    """empty :"""
+    pass
+
+# ===========================
+# ERROR
+# ===========================
+def p_error(p):
+    mensaje = f"Syntax error cerca de: {p.value if p else 'EOF'}"
+    with open(f"./Logs/Sintactico-{lexer.nombre_archivo}-{lexer.fecha_actual}.txt",
+              "a+", encoding="utf-8") as f:
+        f.write(mensaje + "\n")
+
+parser = yacc.yacc(write_tables=False)
+
+def analizador_sintactico(data):
+    return parser.parse(data, lexer=lexer)
